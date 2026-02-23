ce31701961ba1cfefa16d40d9c60157616e1dc8b
Enock <djsean@Enocks-MacBook-Pro.local>
2026-02-23 10:11:27 +0300

    all changes made 2

--- tours_travels/urls.py
+++ tours_travels/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path('health/', health_check, name='health-check'),
     path('admin/', admin.site.urls),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('', include('adminside.urls')),
